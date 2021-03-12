7a445a7cabb1395ac04ea8509f2bcd16e8f73224
roadmonster <roadmonster1989@gmail.com>
2021-03-12 13:12:31 -0800

    fixed bugs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, Response
 import json
 import io
 import zlib
@@ -10,12 +10,12 @@ app = Flask(__name__)
 
 def compress_nparr(nparr):
     bytestream = io.BytesIO()
-    np.save(bytestream,nparr)
+    np.save(bytestream, nparr)
     uncompressed = bytestream.getvalue()
     compressed = zlib.compress(uncompressed)
     return compressed, len(uncompressed), len(compressed)
 
-@app.route('/img/resize', method= ['POST'])
+@app.route('/img/resize', method=['POST'])
 def resize():
     img = request.data
     img_arr = np.fromstring(img, np.uint8)
@@ -29,45 +29,47 @@ def resize():
     width = int(width)
     height = int(height)
 
-    my_img = cv2.imdecode(img_arr,cv2.IMREAD_COLOR)
+    
+    my_img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
 
     my_img = cv2.resize(img, (width, height))
     resp,_,_=compress_nparr(img)
-    return Response(response = resp, status = 200, mimetype = "application/octet_stream")
+    return Response(response=resp, status=200, mimetype="application/octet_stream")
 
 
 
 @app.route('/img/rotate', methods = ['POST'])
 def rotate():
     
-    img = request.data
-    img_arr = np.fromstring(img, unit8)
-
+    bi_img = request.data
+    img_arr = np.fromstring(bi_img, np.uint8)
+    # get params
     angle = request.args.get('angle')
     if angle is None:
-        return Response(response = json.dumps({'msg': 'missing parameters'}), status=400, mimetype="application/json")
-
-    angle = (int)angle
+        return Response(response=json.dumps({'msg': 'missing paramaters'}), status=400, mimetype="application/json")
+    angle = int(angle)
 
     img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
 
     img_center = tuple(np.array(img.shape[1::-1])/2)
     rotate_matrix = cv2.getRotationMatrix2D(img_center, angle, 1)
     img = cv2.warpAffine(img, rotate_matrix, img.shape[1::-1], flags=cv2.INTER_LINEAR)
-    resp,_,_ = compress_nparr(img)
+    resp, _, _ = compress_nparr(img)
     return Response(response=resp, status=200, mimetype="application/octet_stream")
 
-@app.route('/img/flip', methods = ['POST'])
+
+
+@app.route('/img/flip', methods=['POST'])
 def flip():
 
     recv_img = request.data
-    img_arr = np.fromstring(recv_img, uint8)
+    img_arr = np.fromstring(recv_img, np.uint8)
 
     flip_direction = request.args.get('flipDir')
     if flip_direction is None:
-        return Response(response = json.dumps({'msg':'missing parameters'}), status=400), mimetype="application/json"
+        return Response(response=json.dumps({'msg':'missing parameters'}), status=400, mimetype="application/json")
     
-    flip_direction = (int)flip_direction
+    flip_direction = int(flip_direction)
 
     img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
 
@@ -75,12 +77,12 @@ def flip():
 
     resp,_,_ = compress_nparr(img)
 
-    return Response(response = resp, status = 200, mimetype = "application/octet_stream")
+    return Response(response=resp, status=200, mimetype="application/octet_stream")
 
-@app.route('/img/gray', mthods = ['POST'])
+@app.route('/img/gray', mthods=['POST'])
 def gray():
     recv_img = request.data
-    img_arr = np.fromstring(recv_img, uint8)
+    img_arr = np.fromstring(recv_img, np.uint8)
 
     img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
 
@@ -88,7 +90,7 @@ def gray():
 
     resp, _, _ = compress_nparr(img)
 
-    return Response(response= resp, status = 200, mimetype = "application/octet_stream")
+    return Response(response=resp, status=200, mimetype="application/octet_stream")
 
 if __name__ == '__main__':
     app.run()
